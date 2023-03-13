43d6732039ca02bc96010f4ce67cb497f24821d9
Revabindra <122342037+Revabindra@users.noreply.github.com>
2023-03-13 09:11:07 +0530

    Update vowel2.cpp

--- vowel2.cpp
+++ vowel2.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 	char c;
-	printf("Enter a vowel: ");
+	printf("Enter a character : ");
 	scanf("%c",&c);
 	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
 		printf("The character %c is a vowel.\n",c);
